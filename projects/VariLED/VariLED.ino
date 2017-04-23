546c6b4d3656df085339f3e4da3a7cfc65502a0b
ES Teoh <esteoh@gmail.com>
2017-04-23 14:50:54 +0800

    initial commit VariLED

---
+++ projects/VariLED/VariLED.ino
@@ -0,0 +1,19 @@
+// Blink with vary power
+ 
+// Pin 11 PWM has an LED connected.
+// give it a name:
+int led = 11;
+
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);     
+}
+
+void loop() {
+  for (int i = 0 ; i < 256 ; i++)
+  {
+    analogWrite(led, i);
+    delay(100);               // wait for 0.1 second
+  }
+}
+
