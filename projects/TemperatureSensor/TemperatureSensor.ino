17756dcc60ad87ac1d4d9d31ae9886fd7753ed87
ES Teoh <esteoh@gmail.com>
2017-04-23 15:50:55 +0800

    create TemperatureSensor

---
+++ projects/TemperatureSensor/TemperatureSensor.ino
@@ -0,0 +1,20 @@
+//  Analog Input - Temperature
+
+int sensorPin = A0;    // select the input pin for the sensor
+int valueTemp;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  valueTemp = analogRead(sensorPin);    
+  valueTemp = (5.0 * valueTemp * 100.0)/1024.0;
+  Serial.print("Temperature: ");
+  Serial.print(valueTemp);
+  Serial.println(" Celcius");
+  delay(5000);
+                    
+}
+
