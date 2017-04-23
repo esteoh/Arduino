376457f2274c724dee624c7227e04900140b6bee
ES Teoh <esteoh@gmail.com>
2017-04-23 14:39:47 +0800

    first commit

---
+++ projects/RGBLed/RGBLed.ino
@@ -0,0 +1,25 @@
+void setup() {
+
+  for ( int i = 9 ; i <= 11 ; i++ )
+  {
+    pinMode(i,OUTPUT);
+  }
+}
+
+void loop() {
+
+  for ( int i = 0 ; i < 256 ; i++ )
+  {
+    analogWrite(9, i);
+    for ( int j = 0 ; j < 256 ; j++ )
+    {
+      analogWrite(10, j);
+      for ( int k = 0 ; k < 256 ; k++ )
+      {
+        analogWrite(11, k);
+        delayMicroseconds(100);
+      }
+    }
+  }
+  
+}
